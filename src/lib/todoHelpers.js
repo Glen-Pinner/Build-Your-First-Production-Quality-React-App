74c7fd1a89146e54e3656268136a06b140162661
Glen Pinner <glen.pinner@gmail.com>
2017-02-01 10:11:32 +0000

    Lesson 12: Update data in a list without mutations.

--- src/lib/todoHelpers.js
+++ src/lib/todoHelpers.js
@@ -1,3 +1,17 @@
 export const addTodo = (list, item) => [...list, item];
 
 export const generateId = () => Math.floor(Math.random() * 1000000);
+
+export const findById = (id, list) => list.find(item => item.id === id);
+
+// Use spread operator on 'todo' and then overwrite the isComplete property
+export const toggleTodo = (todo) => ({...todo, isComplete: !todo.isComplete});
+
+export const updateTodo = (list, updated) => {
+    const updatedIndex = list.findIndex(item => item.id === updated.id);
+    return [
+        ...list.slice(0, updatedIndex),
+        updated,
+        ...list.slice(updatedIndex + 1)
+    ];
+};
