28787ba5147bc896244fa2a2d50dc33111afa862
Glen Pinner <glen.pinner@gmail.com>
2017-02-01 11:28:38 +0000

    Lesson 15: Remove items from a list without mutations.

--- src/components/todo/TodoItem.js
+++ src/components/todo/TodoItem.js
@@ -5,9 +5,11 @@ export const TodoItem = (props) => {
     // const handleToggle = () => props.handleToggle(props.id);
     // const handleToggle = props.handleToggle.bind(null, props.id);
     const handleToggle = partial(props.handleToggle, props.id);
+    const handleRemove = partial(props.handleRemove, props.id);
 
     return (
         <li>
+            <span className="delete-item"><a href="#" onClick={handleRemove}>x</a></span>
             <input type="checkbox"
                    onChange={handleToggle}
                    checked={props.isComplete} />
