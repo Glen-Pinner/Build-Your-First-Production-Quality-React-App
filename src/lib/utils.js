4e1d0e4e5296d92d9acd8192ed089e39ebc1af48
Glen Pinner <glen.pinner@gmail.com>
2017-02-01 11:07:59 +0000

    Lesson 14: Create a pipe function to enable function composition.

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,1 +1,5 @@
 export const partial = (fn, ...args) => fn.bind(null, ...args); // args to lambda uses spread, args to bind uses rest
+
+const _pipe = (f, g) => (...args) => g(f(...args));
+
+export const pipe = (...fns) => fns.reduce(_pipe);
