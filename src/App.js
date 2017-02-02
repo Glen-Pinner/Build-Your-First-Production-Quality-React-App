caa74ab1b1465afd074697eb97306bb319d99e44
Glen Pinner <glen.pinner@gmail.com>
2017-02-02 10:42:17 +0000

    Lesson 22: Show temporary messages in a React application.

--- src/App.js
+++ src/App.js
@@ -62,7 +62,12 @@ class App extends Component {
 
         // Update on JSON server
         createTodo(newTodo)
-            .then(() => console.log('Todo added'));
+            .then(() => this.showTempMessage('Todo added'));
+    };
+
+    showTempMessage = (msg) => {
+        this.setState({message: msg});
+        setTimeout(() => this.setState({message: ''}), 2500);
     };
 
     handleEmptySubmit = (event) => {
@@ -91,6 +96,7 @@ class App extends Component {
                 </div>
                 <div className="Todo-App">
                     {this.state.errorMessage && <span className="error">{this.state.errorMessage}</span>}
+                    {this.state.message && <span className="success">{this.state.message}</span>}
                     <TodoForm
                         handleInputChange={this.handleInputChange}
                         currentTodo={this.state.currentTodo}
