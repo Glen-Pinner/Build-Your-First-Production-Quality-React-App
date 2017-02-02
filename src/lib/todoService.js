163c5f765b752e09ae2fda72297bd397b85d95c8
Glen Pinner <glen.pinner@gmail.com>
2017-02-02 11:02:53 +0000

    Lesson 24: Delete data on the server with fetch.

--- src/lib/todoService.js
+++ src/lib/todoService.js
@@ -14,7 +14,6 @@ export const createTodo = (todo) => {
         },
         body: JSON.stringify(todo)
     }).then(res => res.json());
-
 };
 
 export const saveTodo = (todo) => {
@@ -26,5 +25,14 @@ export const saveTodo = (todo) => {
         },
         body: JSON.stringify(todo)
     }).then(res => res.json());
+};
 
+export const destroyTodo = (id) => {
+    return fetch(`${baseUrl}/${id}`, {
+        method: 'DELETE',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    });
 };
