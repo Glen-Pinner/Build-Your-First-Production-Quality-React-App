7ba790f21dd2ba3d6a739d922680c7580036d960
Glen Pinner <glen.pinner@gmail.com>
2017-02-02 10:52:12 +0000

    Lesson 23: Update data on the server with fetch.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import {TodoForm, TodoList, Footer} from './components/todo';
 import {addTodo, generateId, findById, toggleTodo, updateTodo, removeTodo, filterTodos} from './lib/todoHelpers';
 import {pipe, partial} from './lib/utils';
-import {loadTodos, createTodo} from './lib/todoService';
+import {loadTodos, createTodo, saveTodo} from './lib/todoService';
 
 
 class App extends Component {
@@ -33,14 +33,20 @@ class App extends Component {
     };
 
     handleToggle = (id) => {
-        const getUpdatedTodos = pipe(findById, toggleTodo, partial(updateTodo, this.state.todos));
-        const updatedTodos = getUpdatedTodos(id, this.state.todos);
+        const getToggledTodo = pipe(findById, toggleTodo);
+        const updated = getToggledTodo(id, this.state.todos);
+        const getUpdatedTodos = partial(updateTodo, this.state.todos);
+        const updatedTodos = getUpdatedTodos(updated);
 
         // const todo = findById(id, this.state.todos);
         // const toggled = toggleTodo(todo);
         // const updatedTodos = updateTodo(this.state.todos, toggled);
 
         this.setState({ todos: updatedTodos });
+
+        // Update on JSON server
+        saveTodo(updated)
+            .then(() => this.showTempMessage('Todo updated'));
     };
 
     handleSubmit = (event) => {
